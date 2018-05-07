010994cc27d520546b1355a4acc00f247d9cf81b
sT <st.email@qq.com>
2018-05-07 16:10:50 +0800

    url修改

--- src/main/url-config.js
+++ src/main/url-config.js
@@ -1,9 +1,10 @@
 /**
  * 系统配置的URL
  */
-const url =`http://114.55.249.156`;
+const debug=false;
+const url = debug ?`http://127.0.0.1`:`http://114.55.249.156`;
 
-const baseUrl = `${url}:9090`;
+const baseUrl = debug ? `${url}:8080`:`${url}:9090`;
 // const baseUrl = `${url}:9090`
 exports.baseUrl = baseUrl;
 //获取公司列表
@@ -15,8 +16,8 @@ exports.needVerifyCode = `${baseUrl}/staff/need_verity_code`;
 //根据公司ID登陆
 exports.loginWithCompanyId = `${baseUrl}/staff/login_with_company_id`;
 //主界面URL
-exports.mainPageUrl = `${url}/login`;
+exports.mainPageUrl = debug ? `${url}:8081/login`:`${url}/login`;
 //领取客资URL
 
 //上下线 
-exports.websocketUrl =`ws://localhost:8989/ws`;
+exports.websocketUrl =`ws://localhost:8030/ws`;
